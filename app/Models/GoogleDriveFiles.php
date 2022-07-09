dc2186cdb7451d1ce4623eef5417898c4c640bfd
HungNguyen <hungnguyen.gifttify@gmail.com>
2022-07-10 03:32:28 +0700

    Use mysql to query files

--- app/Models/GoogleDriveFiles.php
+++ app/Models/GoogleDriveFiles.php
@@ -85,4 +85,45 @@ class GoogleDriveFiles extends Model
         return $result;
     }
 
+    public static function flat_image_links_from_folder_id_by_mysql_query ($folderId) {
+        $deepLevel = 8;
+        $data = DB::select("
+            WITH RECURSIVE google_drive_files_flat
+            AS (
+                  SELECT gdf.id, gdf.`name` , gdf.webContentLink, gdf.mimeType, gdf.parentId, 1 `level`
+                    , @rownum := @rownum + 1 as stt, CONVERT('1', CHAR(100)) as stt_order
+                  from google_drive_files gdf
+                  cross join (select @rownum := 0) r
+                  where gdf.id = :folderId
+
+                  UNION ALL
+
+                  SELECT gdf.id, gdf.`name` , gdf.webContentLink, gdf.mimeType, gdf.parentId, `level`+1
+                    , @rownum := @rownum + 1 as stt
+                    , (case gdf.mimeType when 'application/vnd.google-apps.folder' then CONCAT( CONVERT(gdff.stt_order, CHAR), CONVERT(@rownum, CHAR) ) else CONCAT( CONVERT(gdff.stt_order, CHAR) , LEFT(gdf.`name`,3) ) end )  as stt_order
+                  FROM google_drive_files gdf
+                  INNER JOIN google_drive_files_flat gdff ON gdff.id = gdf.parentId
+                  WHERE `level` < $deepLevel
+                  and gdff.mimeType = 'application/vnd.google-apps.folder'
+                  and gdf.mimeType != 'image/x-photoshop'
+                )
+            select * FROM
+            (
+                SELECT `level`, `name`, id, '' as link, 'folder' as type, stt_order, stt, parentId
+                FROM google_drive_files_flat
+                where mimeType = 'application/vnd.google-apps.folder'
+
+                UNION ALL
+
+                SELECT `level`, `name`, id, REPLACE(webContentLink, '&export=download', '') as link, 'image' as type, stt_order, stt, parentId
+                FROM google_drive_files_flat
+                where mimeType != 'application/vnd.google-apps.folder' and LEFT(UPPER(name), 2) = 'MK'
+            ) a
+            order by stt_order asc, `name` asc;
+            ",
+            ['folderId' => $folderId]
+        );
+        return collect($data)->map(function($x){ return (array) $x; })->toArray();
+    }
+
 }

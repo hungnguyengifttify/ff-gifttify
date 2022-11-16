7181ef625861b705772a2c58ac8f81ec3998b491
hatv <hatv1592@gmail.com>
2022-11-16 16:04:09 +0700

    Update many order

--- app/Console/Commands/UploadOrderToMailChimp.php
+++ app/Console/Commands/UploadOrderToMailChimp.php
@@ -122,6 +122,114 @@ class UploadOrderToMailChimp extends Command
         "paidAt": 1666847437672,
         "expressShipping": false,
         "transactionId": "7F2585833G861140A"
+    }, 
+    {
+        "id": "c3cotpsUd_test",
+        "email": "michaelargent887@gmail.com",
+        "cartId": "o5uLwFHD2Fx",
+        "consentsAdvertising": true,
+        "address": {
+            "phone": "+61418471581",
+            "state": "VIC",
+            "country": "AU",
+            "firstName": "Michael",
+            "lastName": "Argent",
+            "address1": "11 Bendigo avenue elwood",
+            "city": "Melbourne",
+            "postcode": "3184"
+        },
+        "paymentMethod": "paypal",
+        "status": "completed",
+        "createdAt": 1666846770014,
+        "updatedAt": 1666847438128,
+        "billingAddress": {},
+        "sameBillingAddress": true,
+        "items": [
+            {
+                "id": "IudD3cVaDn",
+                "product": {
+                    "id": "ZYoqBKd9W5",
+                    "type": "variable",
+                    "author": "admin",
+                    "slug": "sheep-floral-pattern-casual-shirt",
+                    "shopifyId": "g_634fa3523a7937.805474708258",
+                    "title": "Sheep Floral Pattern Casual Shirt",
+                    "productType": "CS",
+                    "status": "publish",
+                    "tags": "Casual Shirt, uni-TR2491, des-sheep-floral-pattern-191022",
+                    "tagsArr": [
+                        "Casual Shirt",
+                        "uni-TR2491",
+                        "des-sheep-floral-pattern-191022"
+                    ],
+                    "images": [
+                        {
+                            "alt": "",
+                            "src": "https://cdn.shopify.com/s/files/1/0516/6730/4607/products/Mockms.jpg?v=1662433284"
+                        },
+                        {
+                            "alt": "",
+                            "src": "https://cdn.shopify.com/s/files/1/0516/6730/4607/products/14_5fcf2ca3-6fb0-4876-9a4d-749b704f5b4f.jpg?v=1662432103"
+                        }
+                    ],
+                    "seo": {
+                        "title": "Sheep Floral Pattern Casual Shirt",
+                        "description": "Sheep Floral Pattern Casual Shirt"
+                    },
+                    "category": "Clothing",
+                    "gender": "women",
+                    "createdAt": 1666163542154,
+                    "updatedAt": 1666163542154
+                },
+                "variant": {
+                    "id": "gv_634fa3523a7a09.307022406173",
+                    "sku": "220405-i102-03-FARM-CS-14-DES1157",
+                    "image": {
+                        "alt": "",
+                        "src": ""
+                    },
+                    "price": 34.95,
+                    "option1": "Casual Shirt",
+                    "option2": "",
+                    "option3": "",
+                    "quantity": 9999,
+                    "fulfilment": "manual"
+                },
+                "options": {
+                    "Size Casual Shirt": "S"
+                },
+                "viewOptions": [
+                    {
+                        "name": "Type",
+                        "value": "Casual Shirt"
+                    },
+                    {
+                        "name": "Size Casual Shirt",
+                        "value": "S"
+                    }
+                ],
+                "price": 34.95,
+                "delivery": [],
+                "quantity": 2,
+                "subTotal": 69.9
+            }
+        ],
+        "cartTotal": 66.4,
+        "shippingTotal": 7.99,
+        "discount": 3.5,
+        "couponCode": "",
+        "total": 74.39,
+        "currency": {
+            "code": "USD",
+            "symbol": "US$",
+            "rate": 1
+        },
+        "shippingMethod": "Standard Shipping",
+        "sandbox": false,
+        "paypalOrderId": "0PN267367D687992Y",
+        "paidAt": 1666847437672,
+        "expressShipping": false,
+        "transactionId": "7F2585833G861140A"
     }]';
     
     /**
@@ -257,7 +365,7 @@ class UploadOrderToMailChimp extends Command
                 }
             }catch (Exception $e) {
                 if($e->getResponse()->getStatusCode() == 400){
-                    dump('Order đã tồn tại vui lòng xóa hoăc bật xóa order để cập nhâp');
+                    dump('Order đã '.$orderInfo["id"].' tồn tại vui lòng xóa hoăc bật xóa order để cập nhâp');
                 };
             }
         }

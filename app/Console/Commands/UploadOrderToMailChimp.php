a30a6f5f516a5f5ad9da6272439fa37d8af8d014
hatv <hatv1592@gmail.com>
2022-11-17 03:20:59 +0700

    Update order

--- app/Console/Commands/UploadOrderToMailChimp.php
+++ app/Console/Commands/UploadOrderToMailChimp.php
@@ -2,6 +2,8 @@
 
 namespace App\Console\Commands;
 
+use Illuminate\Support\Facades\Log;
+use App\Models\RedisGtf;
 use Illuminate\Console\Command;
 use App\Services\MailChimpService;
 use Exception;
@@ -247,130 +249,201 @@ class UploadOrderToMailChimp extends Command
 
     public function handle()
     {
+        $siteDomain = 'https://thecreattify.com';
         $storeID = 'store_k6gosw5gwhooezt3i61m';
+        $pageOrder = 0;
 
         $mailchimp = new MailChimpService();
-        $listOrdersForImport = json_decode($this->json_order, true);
-     
-        foreach ($listOrdersForImport as $orderInfo) {
-            $listProduct = $this->getOrderItems($orderInfo);
-            foreach($listProduct as $productInfo){
-                try {
+        // $response = $mailchimp->service->ecommerce->stores();
+        do {
+            $pageOrder++;
+            $data = RedisGtf::getRedisOrdersList(1, array('2022-11-01', '2022-11-16'), 'draft', $pageOrder, 100);
+            foreach ($data['results'] as $v) {
+                // Import order
+                $listProduct = $this->getOrderItems($v);
+                foreach($listProduct as $productInfo){
+                    try {
+                        // try {
+                        $mailchimp->service->ecommerce->updateStoreProduct(
+                            $storeID, 
+                            $productInfo['product']["id"],
+                            [
+                                "title" => $productInfo['product']['title'], // REQUIRE
+                                "url" => $siteDomain . '/products/' . $productInfo['product']['slug'] . '-' .  $productInfo['product']['id'],
+                                "type" => $productInfo['product']['type'],
+                                "image_url" => $productInfo['product']['images'][0]['src'] ?? '',
+                            ]
+                        );
+                        // }catch (Exception $e) {}
+                        $variantInfo = [
+                            "id" => $productInfo['variant']['id'], // REQUIRE
+                            "title" => $productInfo['product']['title'], // REQUIRE
+                            "url" => $siteDomain . '/products/' . $productInfo['product']['slug'] . '-' .  $productInfo['product']['id'],
+                            "sku" => $productInfo['variant']['sku'], //The handle of a product.
+                            "price" => $productInfo['variant']['price'],
+                            "image_url" => $productInfo['product']['images'][0]['src'] ?? '',
+                            // "images" => [
+                            //     "variant_ids" => $variantsInfo['listVariantId'] // Danh sách ID nhân ảnh
+                            // ], 
+                            "published_at_foreign" =>  date('c', $productInfo['product']['createdAt']), //The date and time the product was published.
+                        ];
+                        $updatePrdVr = $mailchimp->service->ecommerce->addProductVariant(
+                            $storeID,
+                            $productInfo['product']['id'],
+                            $productInfo['variant']['id'],
+                            $variantInfo
+                        );
+
+                    }catch (Exception $e) {
+                        if($e->getResponse()->getStatusCode() == 404){
+                            try {
+                                $infoProduct = [
+                                    "id" => $productInfo['product']['id'],  // REQUIRE
+                                    "title" => $productInfo['product']['title'], // REQUIRE
+                                    "variants" => [
+                                        [
+                                        'id' =>  $productInfo['variant']['id'],
+                                        'title' => $productInfo['product']['title'],  
+                                        'url' => $siteDomain.'products/' . $productInfo['product']['slug'] . '-' .  $productInfo['product']['id'],
+                                        'sku' => $productInfo['variant']['sku'],
+                                        'price' => $productInfo['variant']['price'],
+                                        'inventory_quantity' => $productInfo['variant']['quantity'],                              
+                                        'image_url' => $productInfo['product']['images'][0]['src'] ?? '',  
+                                        ]
+                                    ], // REQUIRE
+                                    "handle" =>  "API_PUSH", //The handle of a product.
+                                    "url" => $siteDomain.'products/' . $productInfo['product']['slug'] . '-' .  $productInfo['product']['id'],
+                                    "description" => "",
+                                    "type" => $productInfo['product']['productType'],
+                                    "published_at_foreign" =>  date('c', $productInfo['product']['createdAt']), //The date and time the product was published.
+                                ];
+
+                                $importProduct = $mailchimp->service->ecommerce->addStoreProduct(
+                                    $storeID,
+                                    $infoProduct
+                                );
+                            }catch (Exception $e) {
+                                dump($e);
+                            };
+                        };
+                    }
+                }
+
+                // Import Customer
+                $updateCustomer = $mailchimp->service->ecommerce->setStoreCustomer($storeID, $v["email"], [
+                    "id" => $v["email"],
+                    "email_address" => $v["email"],
+                    "opt_in_status" => true,
+                    "first_name" => $v["address"]["firstName"],
+                    "last_name" => $v["address"]["lastName"],
+                    "phone" => $v["address"]["phone"],
+                     "address" => [
+                        'address1' => $v["address"]["address1"] ?? '',
+                        'city' => $v["address"]["city"] ?? '',
+                        'postal_code' => $v["address"]["postcode"] ?? '',
+                        'country_code' => $v["address"]["country"] ?? '',
+                        'province' => $v["address"]["state"] ?? ''
+                    ]
+                ]);
+
+                // Import order or cart
+                if($v["status"] == 'draft'){
+                    // Show all cart
+                    // $mailchimp->service->ecommerce->getStoreCarts($storeID);
                     
-                    $mailchimp->service->ecommerce->getStoreProduct($storeID, $productInfo['product']["id"]);
-                    $variantInfo = [
-                        "title" => $productInfo['product']['title'], // REQUIRE
-                        "url" => 'https://thecreattify.com/' . $productInfo['product']['slug'] . '-' .  $productInfo['product']['id'],
-                        "sku" => $productInfo['variant']['sku'], //The handle of a product.
-                        "price" => $productInfo['variant']['price'],
-                        "image_url" => $productInfo['product']['images'][0]['src'] ?? ''
+                    // Import Cart
+                    $dataInsert = [
+                        "id" => $v["id"],
+                        "customer" => [
+                            "id" => $v["email"], //A unique identifier for the customer. Limited to 50 characters.
+                        ], //Information about a specific customer. For existing customers include only the id parameter in the customer object body.
+                        "currency_code" => $v["currency"]["code"] ?? 'USD',
+                        "order_total" =>  $this->totalPriceDraft($v), // The order total for the cart.
+                        "lines" => $this->getLines($v),
+                        "checkout_url" => $siteDomain . 'checkout/' . $v["id"] // A string that uniquely identifies the campaign for an order.
                     ];
 
-                    $mailchimp->service->ecommerce->updateProductVariant(
-                        $storeID,
-                        $productInfo['product']['id'],
-                        $productInfo['variant']['id'],
-                        $variantInfo
-                    );
-                }catch (Exception $e) {
-                    if($e->getResponse()->getStatusCode() == 404){
-                        try {
-                            $infoProduct = [
-                                "id" => $productInfo['product']['id'],  // REQUIRE
-                                "title" => $productInfo['product']['title'], // REQUIRE
-                                "variants" => [
-                                    [
-                                    'id' =>  $productInfo['variant']['id'],
-                                    'title' => $productInfo['product']['title'],  
-                                    'url' => 'https://thecreattify.com/' . $productInfo['product']['slug'] . '-' .  $productInfo['product']['id'],
-                                    'sku' => $productInfo['variant']['sku'],
-                                    'price' => $productInfo['variant']['price'],
-                                    'inventory_quantity' => $productInfo['variant']['quantity'],
-                                    'image_url' => $productInfo['product']['images'][0]['src'] ?? '',  
-                                    ]
-                                ], // REQUIRE
-                                "handle" =>  "API_PUSH", //The handle of a product.
-                                "url" => 'https://thecreattify.com/' . $productInfo['product']['slug'] . '-' .  $productInfo['product']['id'],
-                                "description" => "",
-                                "type" => $productInfo['product']['productType'],
-                                // "published_at_foreign" =>  $productInfo['product']['createdAt'], //The date and time the product was published.
-                            ];
-                            // dd($infoProduct);
-                            $importProduct = $mailchimp->service->ecommerce->addStoreProduct(
-                                $storeID,
-                                 $infoProduct
-                            );
-                        }catch (Exception $e) {
-                            dump($e);
+                    try {
+                        $addOrder = $mailchimp->service->ecommerce->addStoreCart(
+                            $storeID,
+                            $dataInsert
+                        );
+        
+                        if($addOrder){
+                            dump('Insert cart ID: "'. $v["id"] .'" Thành công cho email: ' . $v["email"]);
+                            Log::notice('Insert cart ID: "'. $v["id"] .'" Thành công cho email: ' . $v["email"]);
+                        }
+                    }catch (Exception $e) {
+                        if($e->getResponse()->getStatusCode() == 400){
+                            dump('Cart '.$v["id"]. ''. $v["email"].' tồn tại vui lòng xóa hoăc bật xóa order để cập nhâp');
+                            Log::notice('Cart '.$v["id"]. ''. $v["email"].' tồn tại vui lòng xóa hoăc bật xóa order để cập nhâp');
+                        }else{
+                            Log::notice($e);
                         };
-                    };
+                    }
+                }else{
+                    try {
+                        $mailchimp->service->ecommerce->deleteOrder($storeID, $v["id"]);
+                        dump('Xóa Order '.$v["id"]. 'Email: ' . $v["email"]);
+                        Log::notice(('Xóa Order '.$v["id"]. 'Email: ' . $v["email"]));
+                    }catch (Exception $e) {
+                    }
+                    $dataInsert = [
+                        "id" => $v["id"],
+                        "customer" => [
+                            "id" => $v["email"], //A unique identifier for the customer. Limited to 50 characters.
+                        ], //Information about a specific customer. For existing customers include only the id parameter in the customer object body.
+                        "currency_code" => $v["currency"]["code"],
+                        "order_total" =>  $v["total"], // The total for the order.
+                        "lines" => $this->getLines($v),
+        
+                        // "campaign_id" // A string that uniquely identifies the campaign for an order.
+                        // "landing_site" // The URL for the page where the buyer landed when entering the shop.
+                       
+                        "financial_status" => 'paid', //paid, pending, refunded, cancelled
+                        "fulfillment_status" => $v["status"],
+                       
+                        "discount_total" => $v["discount"],
+                        "shipping_total" => $v["shippingTotal"],
+                        "processed_at_foreign" => date('c', $v['updatedAt']),
+                        "updated_at_foreign" => date('c',$v['updatedAt']),
+                        // "tracking_code" => $v["transactionId"],
+                        // "processed_at_foreign" //
+                        // "cancelled_at_foreign" 
+                        // "cancelled_at_foreign" 
+                        // "updated_at_foreign"
+                        // "shipping_address"
+                        // "billing_address"
+                        // "promos"
+                        // "outreach"
+                        // "tracking_number"
+                        // "tracking_carrier"
+                        // "tracking_url"
+                    ];
+                    try {
+                        // Insert Order
+                        $addOrder = $mailchimp->service->ecommerce->addStoreOrder(
+                            $storeID,
+                            $dataInsert
+                        );
+                        if($addOrder){
+                            dump('Insert đơn hàng ID: "'. $v["id"] .'" Thành công cho email: ' . $v["email"]);
+                            Log::notice('Insert đơn hàng ID: "'. $v["id"] .'" Thành công cho email: ' . $v["email"]);
+                        }
+                    }catch (Exception $e) {
+                        if($e->getResponse()->getStatusCode() == 400){
+                            dump('Order đã '.$v["id"].' tồn tại vui lòng xóa hoăc bật xóa order để cập nhâp');
+                            Log::notice('Order đã '.$v["id"].' tồn tại vui lòng xóa hoăc bật xóa order để cập nhâp');
+                        }else{
+                            Log::notice($e);
+                        };
+                    }
                 }
             }
-
-            // Update customer
-            $updateCustomer = $mailchimp->service->ecommerce->setStoreCustomer($storeID, $orderInfo["email"], [
-                "id" => $orderInfo["email"],
-                "email_address" => $orderInfo["email"],
-                "opt_in_status" => true,
-                 "address" => [
-                    'address1' => $orderInfo["address"]["address1"] ?? '',
-                    'city' => $orderInfo["address"]["city"] ?? '',
-                    'postal_code' => $orderInfo["address"]["postcode"] ?? '',
-                    'country_code' => $orderInfo["address"]["country"] ?? '',
-                ]
-            ]);
-
-            // Delete Order
-            $a = $mailchimp->service->ecommerce->deleteOrder($storeID, $orderInfo["id"]);
-
-            // Insert Order
-            $dataInsert = [
-                "id" => $orderInfo["id"],
-                "customer" => [
-                    "id" => $orderInfo["email"], //A unique identifier for the customer. Limited to 50 characters.
-                ], //Information about a specific customer. For existing customers include only the id parameter in the customer object body.
-                "currency_code" => $orderInfo["currency"]["code"],
-                "order_total" =>  $orderInfo["total"], // The total for the order.
-                "lines" => $this->getLines($orderInfo),
-
-                // "campaign_id" // A string that uniquely identifies the campaign for an order.
-                // "landing_site" // The URL for the page where the buyer landed when entering the shop.
-               
-                // "financial_status" => $orderInfo["status"], //paid, pending, refunded, cancelled
-                "fulfillment_status" => $orderInfo["status"],
-               
-                "discount_total" => $orderInfo["discount"],
-                "shipping_total" => $orderInfo["shippingTotal"],
-                // "tracking_code" => $orderInfo["transactionId"],
-                // "processed_at_foreign" //
-                // "cancelled_at_foreign" 
-                // "updated_at_foreign"
-                // "shipping_address"
-                // "billing_address"
-                // "promos"
-                // "outreach"
-                // "tracking_number"
-                // "tracking_carrier"
-                // "tracking_url"
-            ];
-            try {
-                // Insert Order
-                $addOrder = $mailchimp->service->ecommerce->addStoreOrder(
-                    $storeID,
-                    $dataInsert
-                );
-
-                if($addOrder){
-                    dump('Insert đơn hàng ID: "'. $orderInfo["id"] .'" Thành công cho email:' . $orderInfo["email"]);
-                }
-            }catch (Exception $e) {
-                if($e->getResponse()->getStatusCode() == 400){
-                    dump('Order đã '.$orderInfo["id"].' tồn tại vui lòng xóa hoăc bật xóa order để cập nhâp');
-                };
-            }
-        }
+            break;
+        } while (empty($data) === false);
+        // sleep(10);
     }
-
 
     public function getOrderItems($order){
         $items = [];
@@ -385,7 +458,7 @@ class UploadOrderToMailChimp extends Command
     public function getLines($order){
         $line = [];
         if(count($order['items'])){
-            foreach($order['items'] as $key => $item){
+            foreach($order['items'] as $item){
                 $tempItem = [];
                 $tempItem['id'] = $item['id'];
                 $tempItem['product_id'] = $item['product']['id'];
@@ -397,5 +470,15 @@ class UploadOrderToMailChimp extends Command
             }
         }
         return $line;
+    }
+
+    public function totalPriceDraft($order){
+        $priceDraft = 0;
+        if(count($this->getLines($order))){
+            foreach($this->getLines($order) as $value){
+                $priceDraft+= $value['price'] ?? 0;
+            }
+        }
+        return $priceDraft;
     }
 }

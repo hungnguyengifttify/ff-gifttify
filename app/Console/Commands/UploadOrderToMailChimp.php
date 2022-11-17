fe6f5840e4cb182e152c6474d53bc30b5c0553e0
hatv <hatv1592@gmail.com>
2022-11-17 15:24:25 +0700

    Update mail chimp

--- app/Console/Commands/UploadOrderToMailChimp.php
+++ app/Console/Commands/UploadOrderToMailChimp.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Log;
 use App\Models\RedisGtf;
 use Illuminate\Console\Command;
 use App\Services\MailChimpService;
+use Carbon\Carbon;
 use Exception;
 
 class UploadOrderToMailChimp extends Command
@@ -17,223 +18,6 @@ class UploadOrderToMailChimp extends Command
      */
     protected $signature = 'mailchimp:push_order';
 
-    protected $json_order = '[{
-        "id": "c3cotpsUd",
-        "email": "michaelargent887@gmail.com",
-        "cartId": "o5uLwFHD2Fx",
-        "consentsAdvertising": true,
-        "address": {
-            "phone": "+61418471581",
-            "state": "VIC",
-            "country": "AU",
-            "firstName": "Michael",
-            "lastName": "Argent",
-            "address1": "11 Bendigo avenue elwood",
-            "city": "Melbourne",
-            "postcode": "3184"
-        },
-        "paymentMethod": "paypal",
-        "status": "completed",
-        "createdAt": 1666846770014,
-        "updatedAt": 1666847438128,
-        "billingAddress": {},
-        "sameBillingAddress": true,
-        "items": [
-            {
-                "id": "IudD3cVaDn",
-                "product": {
-                    "id": "ZYoqBKd9W5",
-                    "type": "variable",
-                    "author": "admin",
-                    "slug": "sheep-floral-pattern-casual-shirt",
-                    "shopifyId": "g_634fa3523a7937.805474708258",
-                    "title": "Sheep Floral Pattern Casual Shirt",
-                    "productType": "CS",
-                    "status": "publish",
-                    "tags": "Casual Shirt, uni-TR2491, des-sheep-floral-pattern-191022",
-                    "tagsArr": [
-                        "Casual Shirt",
-                        "uni-TR2491",
-                        "des-sheep-floral-pattern-191022"
-                    ],
-                    "images": [
-                        {
-                            "alt": "",
-                            "src": "https://cdn.shopify.com/s/files/1/0516/6730/4607/products/Mockms.jpg?v=1662433284"
-                        },
-                        {
-                            "alt": "",
-                            "src": "https://cdn.shopify.com/s/files/1/0516/6730/4607/products/14_5fcf2ca3-6fb0-4876-9a4d-749b704f5b4f.jpg?v=1662432103"
-                        }
-                    ],
-                    "seo": {
-                        "title": "Sheep Floral Pattern Casual Shirt",
-                        "description": "Sheep Floral Pattern Casual Shirt"
-                    },
-                    "category": "Clothing",
-                    "gender": "women",
-                    "createdAt": 1666163542154,
-                    "updatedAt": 1666163542154
-                },
-                "variant": {
-                    "id": "gv_634fa3523a7a09.307022406173",
-                    "sku": "220405-i102-03-FARM-CS-14-DES1157",
-                    "image": {
-                        "alt": "",
-                        "src": ""
-                    },
-                    "price": 34.95,
-                    "option1": "Casual Shirt",
-                    "option2": "",
-                    "option3": "",
-                    "quantity": 9999,
-                    "fulfilment": "manual"
-                },
-                "options": {
-                    "Size Casual Shirt": "S"
-                },
-                "viewOptions": [
-                    {
-                        "name": "Type",
-                        "value": "Casual Shirt"
-                    },
-                    {
-                        "name": "Size Casual Shirt",
-                        "value": "S"
-                    }
-                ],
-                "price": 34.95,
-                "delivery": [],
-                "quantity": 2,
-                "subTotal": 69.9
-            }
-        ],
-        "cartTotal": 66.4,
-        "shippingTotal": 7.99,
-        "discount": 3.5,
-        "couponCode": "",
-        "total": 74.39,
-        "currency": {
-            "code": "USD",
-            "symbol": "US$",
-            "rate": 1
-        },
-        "shippingMethod": "Standard Shipping",
-        "sandbox": false,
-        "paypalOrderId": "0PN267367D687992Y",
-        "paidAt": 1666847437672,
-        "expressShipping": false,
-        "transactionId": "7F2585833G861140A"
-    }, 
-    {
-        "id": "c3cotpsUd_test",
-        "email": "michaelargent887@gmail.com",
-        "cartId": "o5uLwFHD2Fx",
-        "consentsAdvertising": true,
-        "address": {
-            "phone": "+61418471581",
-            "state": "VIC",
-            "country": "AU",
-            "firstName": "Michael",
-            "lastName": "Argent",
-            "address1": "11 Bendigo avenue elwood",
-            "city": "Melbourne",
-            "postcode": "3184"
-        },
-        "paymentMethod": "paypal",
-        "status": "completed",
-        "createdAt": 1666846770014,
-        "updatedAt": 1666847438128,
-        "billingAddress": {},
-        "sameBillingAddress": true,
-        "items": [
-            {
-                "id": "IudD3cVaDn",
-                "product": {
-                    "id": "ZYoqBKd9W5",
-                    "type": "variable",
-                    "author": "admin",
-                    "slug": "sheep-floral-pattern-casual-shirt",
-                    "shopifyId": "g_634fa3523a7937.805474708258",
-                    "title": "Sheep Floral Pattern Casual Shirt",
-                    "productType": "CS",
-                    "status": "publish",
-                    "tags": "Casual Shirt, uni-TR2491, des-sheep-floral-pattern-191022",
-                    "tagsArr": [
-                        "Casual Shirt",
-                        "uni-TR2491",
-                        "des-sheep-floral-pattern-191022"
-                    ],
-                    "images": [
-                        {
-                            "alt": "",
-                            "src": "https://cdn.shopify.com/s/files/1/0516/6730/4607/products/Mockms.jpg?v=1662433284"
-                        },
-                        {
-                            "alt": "",
-                            "src": "https://cdn.shopify.com/s/files/1/0516/6730/4607/products/14_5fcf2ca3-6fb0-4876-9a4d-749b704f5b4f.jpg?v=1662432103"
-                        }
-                    ],
-                    "seo": {
-                        "title": "Sheep Floral Pattern Casual Shirt",
-                        "description": "Sheep Floral Pattern Casual Shirt"
-                    },
-                    "category": "Clothing",
-                    "gender": "women",
-                    "createdAt": 1666163542154,
-                    "updatedAt": 1666163542154
-                },
-                "variant": {
-                    "id": "gv_634fa3523a7a09.307022406173",
-                    "sku": "220405-i102-03-FARM-CS-14-DES1157",
-                    "image": {
-                        "alt": "",
-                        "src": ""
-                    },
-                    "price": 34.95,
-                    "option1": "Casual Shirt",
-                    "option2": "",
-                    "option3": "",
-                    "quantity": 9999,
-                    "fulfilment": "manual"
-                },
-                "options": {
-                    "Size Casual Shirt": "S"
-                },
-                "viewOptions": [
-                    {
-                        "name": "Type",
-                        "value": "Casual Shirt"
-                    },
-                    {
-                        "name": "Size Casual Shirt",
-                        "value": "S"
-                    }
-                ],
-                "price": 34.95,
-                "delivery": [],
-                "quantity": 2,
-                "subTotal": 69.9
-            }
-        ],
-        "cartTotal": 66.4,
-        "shippingTotal": 7.99,
-        "discount": 3.5,
-        "couponCode": "",
-        "total": 74.39,
-        "currency": {
-            "code": "USD",
-            "symbol": "US$",
-            "rate": 1
-        },
-        "shippingMethod": "Standard Shipping",
-        "sandbox": false,
-        "paypalOrderId": "0PN267367D687992Y",
-        "paidAt": 1666847437672,
-        "expressShipping": false,
-        "transactionId": "7F2585833G861140A"
-    }]';
-    
     /**
      * The console command description.
      *
@@ -255,9 +39,12 @@ class UploadOrderToMailChimp extends Command
 
         $mailchimp = new MailChimpService();
         // $response = $mailchimp->service->ecommerce->stores();
+        // $a = $mailchimp->service->ecommerce->deleteStoreCart($storeID, 'TC_JWYIcY1Zq');
+        // $b = $mailchimp->service->ecommerce->getStoreCart($storeID, 'TC_JWYIcY1Zq');
+        // dd($b);
         do {
             $pageOrder++;
-            $data = RedisGtf::getRedisOrdersList(1, array('2022-11-01', '2022-11-16'), 'draft', $pageOrder, 100);
+            $data = RedisGtf::getRedisOrdersList(1, array('2022-11-01', '2022-11-16'), 'complete', $pageOrder, 20);
             foreach ($data['results'] as $v) {
                 // Import order
                 $listProduct = $this->getOrderItems($v);
@@ -330,6 +117,7 @@ class UploadOrderToMailChimp extends Command
                 }
 
                 // Import Customer
+                try{
                 $updateCustomer = $mailchimp->service->ecommerce->setStoreCustomer($storeID, $v["email"], [
                     "id" => $v["email"],
                     "email_address" => $v["email"],
@@ -337,7 +125,7 @@ class UploadOrderToMailChimp extends Command
                     "first_name" => $v["address"]["firstName"],
                     "last_name" => $v["address"]["lastName"],
                     "phone" => $v["address"]["phone"],
-                     "address" => [
+                    "address" => [
                         'address1' => $v["address"]["address1"] ?? '',
                         'city' => $v["address"]["city"] ?? '',
                         'postal_code' => $v["address"]["postcode"] ?? '',
@@ -345,13 +133,17 @@ class UploadOrderToMailChimp extends Command
                         'province' => $v["address"]["state"] ?? ''
                     ]
                 ]);
+                }catch(Exception $e){
+                    dump('Insert/update : "'. $v["id"] .'" không thành công email: ' . $v["email"]);
+                    Log::notice('Insert/update: "'. $v["id"] .'" không thành công cho email: ' . $v["email"]);
+                }
 
                 // Import order or cart
                 if($v["status"] == 'draft'){
                     // Show all cart
                     // $mailchimp->service->ecommerce->getStoreCarts($storeID);
                     
-                    // Import Cart
+                    // Import or update Cart
                     $dataInsert = [
                         "id" => $v["id"],
                         "customer" => [
@@ -360,7 +152,7 @@ class UploadOrderToMailChimp extends Command
                         "currency_code" => $v["currency"]["code"] ?? 'USD',
                         "order_total" =>  $this->totalPriceDraft($v), // The order total for the cart.
                         "lines" => $this->getLines($v),
-                        "checkout_url" => $siteDomain . 'checkout/' . $v["id"] // A string that uniquely identifies the campaign for an order.
+                        "checkout_url" => $siteDomain . '/checkout/' . $v["id"] // A string that uniquely identifies the campaign for an order.
                     ];
 
                     try {
@@ -375,19 +167,20 @@ class UploadOrderToMailChimp extends Command
                         }
                     }catch (Exception $e) {
                         if($e->getResponse()->getStatusCode() == 400){
-                            dump('Cart '.$v["id"]. ''. $v["email"].' tồn tại vui lòng xóa hoăc bật xóa order để cập nhâp');
-                            Log::notice('Cart '.$v["id"]. ''. $v["email"].' tồn tại vui lòng xóa hoăc bật xóa order để cập nhâp');
+                            dump('Cart '.$v["id"]. ''. $v["email"].' tồn tại.');
+                            Log::notice('Cart '.$v["id"]. ''. $v["email"].' tồn tại.');
                         }else{
                             Log::notice($e);
                         };
                     }
                 }else{
+                    // Get order and check exits
                     try {
-                        $mailchimp->service->ecommerce->deleteOrder($storeID, $v["id"]);
-                        dump('Xóa Order '.$v["id"]. 'Email: ' . $v["email"]);
-                        Log::notice(('Xóa Order '.$v["id"]. 'Email: ' . $v["email"]));
+                        $isExitsOrder = $mailchimp->service->ecommerce->getOrder($storeID, $v["id"]);
                     }catch (Exception $e) {
+                        $isExitsOrder = false;
                     }
+                    //
                     $dataInsert = [
                         "id" => $v["id"],
                         "customer" => [
@@ -405,8 +198,8 @@ class UploadOrderToMailChimp extends Command
                        
                         "discount_total" => $v["discount"],
                         "shipping_total" => $v["shippingTotal"],
-                        "processed_at_foreign" => date('c', $v['updatedAt']),
-                        "updated_at_foreign" => date('c',$v['updatedAt']),
+                        "processed_at_foreign" => Carbon::createFromTimestamp($v['updatedAt'])->toDateTimeString(),
+                        "updated_at_foreign" => Carbon::createFromTimestamp($v['updatedAt'])->toDateTimeString(),
                         // "tracking_code" => $v["transactionId"],
                         // "processed_at_foreign" //
                         // "cancelled_at_foreign" 
@@ -422,21 +215,25 @@ class UploadOrderToMailChimp extends Command
                     ];
                     try {
                         // Insert Order
-                        $addOrder = $mailchimp->service->ecommerce->addStoreOrder(
-                            $storeID,
-                            $dataInsert
-                        );
-                        if($addOrder){
+                        if(!$isExitsOrder){
+                            $response =  $mailchimp->service->ecommerce->addStoreOrder(
+                                $storeID,
+                                $dataInsert
+                            );
                             dump('Insert đơn hàng ID: "'. $v["id"] .'" Thành công cho email: ' . $v["email"]);
                             Log::notice('Insert đơn hàng ID: "'. $v["id"] .'" Thành công cho email: ' . $v["email"]);
-                        }
-                    }catch (Exception $e) {
-                        if($e->getResponse()->getStatusCode() == 400){
-                            dump('Order đã '.$v["id"].' tồn tại vui lòng xóa hoăc bật xóa order để cập nhâp');
-                            Log::notice('Order đã '.$v["id"].' tồn tại vui lòng xóa hoăc bật xóa order để cập nhâp');
                         }else{
-                            Log::notice($e);
-                        };
+                            $response = $mailchimp->service->ecommerce->updateOrder(
+                                $storeID,
+                                $v["id"],
+                                $dataInsert
+                            );
+                            dump('Update đơn hàng ID: "'. $v["id"] .'" Thành công cho email: ' . $v["email"]);
+                            Log::notice('Update đơn hàng ID: "'. $v["id"] .'" Thành công cho email: ' . $v["email"]);
+                        }
+                    }catch (Exception $e) {     
+                        dump('Insert/update order '.$v["id"].' email: ' . $v["email"].' thất bại');
+                        Log::notice('Insert/update  order '.$v["id"].' email: ' . $v["email"].' thất bại');
                     }
                 }
             }

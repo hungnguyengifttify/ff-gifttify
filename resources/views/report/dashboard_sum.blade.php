60cdd7734d18b887743a24d53d8986b971671e54
HungNguyen <hungnguyen.gifttify@gmail.com>
2022-12-05 15:03:03 +0700

    Filter date sumury report

--- resources/views/report/dashboard_sum.blade.php
+++ resources/views/report/dashboard_sum.blade.php
@@ -12,6 +12,88 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <form id="reportrangeform" method="get">
+                <div class="bg-white overflow-hidden shadow-sm">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <a id="reportrange" class="btn btn-primary" href="#">
+                            <i class="fa fa-calendar"></i>&nbsp;
+                            <span>{!! $params['labelDate']; !!} </span> <i class="fa fa-caret-down"></i>
+                        </a>
+                        <input type="hidden" name="fromDate" id="fromDate" />
+                        <input type="hidden" name="toDate" id="toDate" />
+                        <input type="hidden" name="labelDate" id="labelDate" />
+                        <input type="hidden" name="debug" id="debug" value="{!! $_REQUEST['debug'] ?? '0';  !!} " />
+                        <span id="reportrangetext">{!! $params['fromDate']->format('d-m-Y') . ' => ' . $params['toDate']->format('d-m-Y'); !!} </span>
+
+                        <script type="text/javascript">
+                            moment.tz.setDefault("America/Los_Angeles");
+                            moment().startOf('isoWeek');
+
+                            $(function() {
+
+                                var url_string = window.location.href;
+                                var url = new URL(url_string);
+                                var fromDate = url.searchParams.get("fromDate");
+                                var toDate = url.searchParams.get("toDate");
+
+                                if (fromDate) {
+                                    var start = moment(fromDate);
+                                    var end = moment(toDate);
+                                    var label = url.searchParams.get("labelDate");
+                                } else {
+                                    var start = moment();
+                                    var end = moment();
+                                    var label = 'Today';
+                                }
+
+                                $('#reportrange').click(function () {
+                                    return false;
+                                });
+
+                                $('#reportrange').daterangepicker({
+                                    "autoApply": true,
+                                    ranges: {
+                                        'Today': [moment(), moment()],
+                                        'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                                        'Last 7 Days': [moment().subtract(7, 'days'), moment()],
+                                        'This Week': [moment().startOf('isoWeek'), moment().endOf('isoWeek')],
+                                        'Last Week': [moment().subtract(7, 'days').startOf('isoWeek'), moment().subtract(7, 'days').endOf('isoWeek')],
+                                        'This Month': [moment().startOf('month'), moment().endOf('month')],
+                                    },
+                                    "alwaysShowCalendars": true,
+                                    startDate: start,
+                                    endDate: end
+                                }, displayDate);
+
+                                function displayDate(start, end, label, isFirstTime) {
+                                    $('#reportrange span').html(label);
+                                    $('#reportrangetext').html(start.format('DD-MM-YYYY') + ' => ' + end.format('DD-MM-YYYY'));
+
+                                    $('#fromDate').val(start.format('YYYY-MM-DD'));
+                                    $('#toDate').val(end.format('YYYY-MM-DD'));
+                                    $('#labelDate').val(label);
+
+                                    console.log($('#fromDate').val());
+                                    console.log($('#toDate').val());
+                                    console.log(isFirstTime);
+
+                                    if (!isFirstTime) {
+                                        $('#reportrangeform').submit();
+                                    }
+                                };
+
+                                displayDate(start, end, label, 1);
+
+                            });
+                        </script>
+                    </div>
+                </div>
+
+            </form>
+
+        </div>
+
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <table class="table table-responsive table-bordered" style="width: auto">
@@ -64,7 +146,7 @@
                             </tr>
                             </thead>
                             <tbody>
-                            <?php foreach ($reports[$st] as $v): ?>
+                                <?php foreach ($reports[$st] as $v): ?>
                             <tr>
                                 <td><?php echo '<b>' . $v['title'] . '</b>' . '<br/>' . "<span class='small'>{$v['dateDisplay']}</span>"; ?></br></td>
                                 <td><?php echo $v['orders']['total']; ?></td>
